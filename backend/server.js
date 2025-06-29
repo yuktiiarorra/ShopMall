b5604f16adc5a8efa33e05a9ca9fae7300498735
yuktiiarorra <arorayukti1919@gmail.com>
2025-06-29 09:45:18 +0530

    Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT || 5000;
+
+connectDB(); // connect to MongoDB
 
 const app = express();
 
@@ -12,14 +15,10 @@ app.get('/', (req, res) => {
     res.send('API is running...;');
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}`)
