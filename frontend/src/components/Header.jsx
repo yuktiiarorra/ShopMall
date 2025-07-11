aad65c88e7fb653ffb96198cd1833c8da898aa7f
yuktiiarorra <arorayukti1919@gmail.com>
2025-07-11 14:47:32 +0530

    Added Backend Authentication

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,43 +1,49 @@
 import React from 'react'
-import { Navbar, Nav, Container} from 'react-bootstrap'
-import { FaShoppingCart, FaUser} from 'react-icons/fa'
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
 // import { LinkContainer } from 'react-router-bootstrap'
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png'
 
 const Header = () => {
-  return (
-    <header>
-        <Navbar bg ="dark" variant="dark" expand="md" collapseOnSelect>
-            <Container>
-                {/* <LinkContainer to='/'> */}
+
+    const { cartItems } = useSelector((state) => state.cart);
+
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+                <Container>
+                    {/* <LinkContainer to='/'> */}
                     {/* <Navbar.Brand href='/'> */}
                     <Navbar.Brand as={Link} to='/'>
                         <img src={logo} alt="ShopMall" />
                         ShopMall
                     </Navbar.Brand>
-                {/* </LinkContainer> */}
-                <Navbar.Toggle aria-controls = "basic-navbar-nav" />
-                <Navbar.Collapse id='basic-navbar-nav'>
+                    {/* </LinkContainer> */}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
-                            {/* <LinkContainer to='/cart'> */}
-                                {/* <Nav.Link href="/cart"> */}
-                                <Nav.Link as={Link} to='/cart'>
-                                    <FaShoppingCart /> Cart
-                                </Nav.Link>
-                            {/* </LinkContainer> */}
-                            {/* <LinkContainer to="/login"> */}
-                                {/* <Nav.Link href="/login"> */}
-                                <Nav.Link as={Link} to='/login'>
-                                    <FaUser /> Sign In
-                                </Nav.Link>
-                            {/* </LinkContainer> */}
+                            <Nav.Link as={Link} to='/cart'>
+                                <FaShoppingCart /> Cart
+                                {
+                                    cartItems.length > 0 && (
+                                        <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                                            {cartItems.reduce((accumulator, currentItem) => accumulator + currentItem.qty, 0)}
+                                        </Badge>
+                                    )
+                                }
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='/login'>
+                                <FaUser /> Sign In
+                            </Nav.Link>
+
                         </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </header>
-  )
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
